bc32e697590e925fc36eb43c88bdaa3b4c525e7c
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-05-10 23:36:33 -0300

    adding give food now button style

--- mobile/src/pages/Home/index.jsx
+++ mobile/src/pages/Home/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 export const Home = () => {
+  function handleGiveFoodNow() {
+    console.log('teste');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Home!</Text>
+      <TouchableOpacity style={styles.giveFoodNowButton} onPress={handleGiveFoodNow}>
+        <Text style={styles.giveFoodNowText}>Alimentar o Pet agora</Text>
+      </TouchableOpacity>
     </View>
   )   
 }
--- mobile/src/pages/Home/styles.js
+++ mobile/src/pages/Home/styles.js
@@ -4,6 +4,35 @@ export default StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center', 
-    alignItems: 'center'
+    alignItems: 'center',
+
+    paddingHorizontal: 50
   },
+
+  giveFoodNowButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    width: '100%',
+    height: 100,
+
+    backgroundColor: '#00b803',
+    borderRadius: 50,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+
+  giveFoodNowText: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold'
+  }
 });
