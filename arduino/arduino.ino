1f4028df310076973941b13c3a20aa8a70afb055
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-05-20 21:56:46 -0300

    setup arduino to send http request to the server

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,36 +1,21 @@
 #include "WiFiEsp.h"
-#include "SoftwareSerial.h"
-#include <Servo.h>
-#include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <Servo.h>
 
-#define ORG "p4b5ix" // Organization ID
-#define DEVICE_TYPE "arduinomegawifi" // Device type
-#define DEVICE_ID "arduinomegawifi01" // Device ID
-#define TOKEN "S&aEMl*qNarGDl&++g" // Authentication token
-
-#define WIFI_SSID "Alexandre" // your network SSID (name)
-#define WIFI_PASSWORD "14241201" // your network password
+#define WIFI_SSID "" // your network SSID (name)
+#define WIFI_PASSWORD "" // your network password
 
 Servo servoMotor;
 WiFiEspClient espClient;
 
-char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
-char authenticationMethod[] = "use-token-auth";
-char token[] = TOKEN;
-char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
-
-const char subscribeTopic[] = "iot-2/cmd/commandR1/fmt/json";
+char server[] = ""; // server address
+int port = 0; // server port
 
 int status = WL_IDLE_STATUS; // the Wifi radio's status
-
 const int servoDigitalPin = 6; //Digital pin used by the servo motor
 
-void callback(char* publishTopic, uint8_t* payload, unsigned int payloadLength);
-
-PubSubClient mqttClient(server, 1883, callback, espClient);
-
-int counter = 0;
+unsigned long lastConnectionTime = 0; // last time you connected to the server, in milliseconds
+const unsigned long postingInterval = 1000; // delay between updates, in milliseconds
 
 void setup() {
   // initialize serial for debugging
@@ -39,24 +24,25 @@ void setup() {
   // initialize serial for ESP module
   Serial3.begin(115200);
 
-  configureWifiConnection();
-
-  connectToTheIBMCloudMQTTWhenNotConected();
+  setupWifiConnection();
     
-  servoMotor.attach(servoDigitalPin); // Association of digital pin to object of Servo type
-  servoMotor.write(0); // Starts the servo motor at 0 degrees
+//  servoMotor.attach(servoDigitalPin); // Association of digital pin to object of Servo type
+//  servoMotor.write(0); // Starts the servo motor at 0 degrees
 }
 
 void loop() { 
-  connectToTheIBMCloudMQTTWhenNotConected();
-  
-//  delay(3000);
-//  servoMotor.write(90); // Rotates at 90 degrees
-//  delay(3000);
-//  servoMotor.write(0); // Returns to 0 degrees
+  while (espClient.available()) {
+    handleHttpResponse();
+  }
+
+  // if postingInterval seconds have passed since your last connection,
+  // then connect again and send data
+  if (millis() - lastConnectionTime > postingInterval) {
+    httpRequest();
+  }
 }
 
-void configureWifiConnection() {
+void setupWifiConnection() {
   // initialize ESP module
   WiFi.init(&Serial3);
 
@@ -98,85 +84,50 @@ void printWifiStatus() {
   Serial.println(" dBm");
 }
 
-void callback(char* publishTopic, uint8_t* payload, unsigned int length) {
-  Serial.println("callback invoked");
-}
+void httpRequest() {
+  // close any connection before send a new request
+  // this will free the socket on the WiFi shield
+  espClient.stop();
 
-void connectToTheIBMCloudMQTTWhenNotConected() {
-  if (!!!mqttClient.connected()) {
-    Serial.print("Reconnecting client to ");
-    Serial.println(server);
-    while (!!!mqttClient.connect(clientId, authenticationMethod, token)) {
-      mqttClient.subscribe(subscribeTopic);
-      Serial.print(".");
-      delay(500);
-    }
-    Serial.println();
+  // if there's a successful connection
+  if (espClient.connect(server, port)) {
+    Serial.println("Connecting...");    
+    
+    // send the HTTP PUT request
+    espClient.println(F("GET /info HTTP/1.1"));
+    espClient.println("Host: " + String(server));
+    espClient.println("Connection: close");
+    espClient.println();
+
+    // note the time that the connection was made
+    lastConnectionTime = millis();
+  }
+  else {
+    // if you couldn't make a connection
+    Serial.println("Connection failed");
   }
 }
 
-//void loop() {
-//  while (client.available()) {
-//    handleHttpResponse();
-//  }
-//
-//  // if postingInterval seconds have passed since your last connection,
-//  // then connect again and send data
-//  if (millis() - lastConnectionTime > postingInterval) {
-//    httpRequest();
-//  }
-//}
-//
-//// this method makes a HTTP connection to the server
-//void httpRequest() {
-//  Serial.println();
-//    
-//  // close any connection before send a new request
-//  // this will free the socket on the WiFi shield
-//  client.stop();
-//
-//  // if there's a successful connection
-//  if (client.connect(server, port)) {
-//    Serial.println("Connecting...");    
-//    
-//    // send the HTTP PUT request
-//    client.println(F("GET /actions HTTP/1.1"));
-//    client.println("Host: " + String(server));
-//    client.println("Connection: close");
-//    client.println();
-//
-//    // note the time that the connection was made
-//    lastConnectionTime = millis();
-//  }
-//  else {
-//    // if you couldn't make a connection
-//    Serial.println("Connection failed");
-//  }
-//}
-//
-//void handleHttpResponse() {
-//  // ignore headers and read to first json bracket
-//  client.readStringUntil('{');
-//
-//  // get json body (everything inside of the main brackets)
-//  String jsonStrWithoutBrackets = client.readStringUntil('}');
-//
-//  // Append brackets to make the string parseable as json
-//  String jsonStr = "{" + jsonStrWithoutBrackets + "}";
-//
-//  Serial.println(jsonStr);
-//
-//  if (jsonStr.indexOf('{', 0) >= 0) {
-//    const size_t bufferSize = JSON_OBJECT_SIZE(1) + 20;
-//    DynamicJsonBuffer jsonBuffer(bufferSize);
-//
-//    JsonObject &responseJson = jsonBuffer.parseObject(jsonStr);
-//
-//    // get and print the value of the action key in our json object
-//    const char *action = responseJson["action"];
-//    const char *plug = responseJson["plug"];
-//    
-//    Serial.println("action -> " + String(action));
-//    Serial.println("plug -> " + String(plug));
-//  }
-//}
+void handleHttpResponse() {
+  // ignore headers and read to first json bracket
+  espClient.readStringUntil('{');
+
+  // get json body (everything inside of the main brackets)
+  String jsonStrWithoutBrackets = espClient.readStringUntil('}');
+
+  // Append brackets to make the string parseable as json
+  String jsonStr = "{" + jsonStrWithoutBrackets + "}";
+
+  Serial.println(jsonStr);
+
+  if (jsonStr.indexOf('{', 0) >= 0) {
+    const size_t bufferSize = JSON_OBJECT_SIZE(1) + 20;
+    DynamicJsonBuffer jsonBuffer(bufferSize);
+
+    JsonObject &responseJson = jsonBuffer.parseObject(jsonStr);
+    
+    const char *serveVersion = responseJson["version"];
+    
+    Serial.println("serveVersion -> " + String(serveVersion));    
+  }
+}
