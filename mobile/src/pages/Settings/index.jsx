dcca07d3e8764e0838ccc17483f700df15f1a842
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-05-21 00:48:25 -0300

    styling the settings page

--- mobile/src/pages/Settings/index.jsx
+++ mobile/src/pages/Settings/index.jsx
@@ -1,13 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 import styles from './styles';
 
 export const Settings = () => {
+  const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+  const [times, setTimes] = useState([]);
+  const [currentDate, setCurrentDate] = useState(new Date());  
+  const [isEdit, setIsEdit] = useState(false);
+  const [editIndex, setEditIndex] = useState(-1);
+
+  function showTimePicker() {
+    setTimePickerVisibility(true);
+  };
+
+  function hideTimePicker() {
+    setTimePickerVisibility(false);
+  };  
+
+  function handleAddNewHour() {
+    if (times.length < 3) {
+      setCurrentDate(new Date());
+      showTimePicker();
+    }
+  }
+
+  function handleEditHour(time, index) {
+    setCurrentDate(new Date(time));
+    setIsEdit(true);
+    setEditIndex(index);
+    showTimePicker();
+  }
+
+  function handleDeleteHour(index) {
+    const newTimes = [...times];
+
+    newTimes.splice(index, 1);
+
+    setTimes(newTimes);
+  }
+
+  function handleTimePickerConfirm(date) {
+    if (isEdit) {
+      const newTimes = times.map((time, index) => {
+        if (index === editIndex) {
+          time = date;
+        }
+
+        return time;
+      });
+
+      setIsEdit(false);
+      setEditIndex(-1);
+      setTimes(newTimes);
+    } else {
+      setTimes([...times, date]);
+    }
+
+    hideTimePicker();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Settings!</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Horários</Text>
+        
+        <TouchableOpacity style={styles.addNewHour} onPress={handleAddNewHour}>
+          <Text style={styles.addNewHourText}>Adicionar novo Horário</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.containerHours}>
+        {times.length === 0 && (
+          <Text style={styles.noDataFoundText}>
+            Nenhum horário cadastrado                      
+          </Text>
+        )}
+
+        {times.length > 0 && times.map((time, index) => (
+          <View key={Math.random() * index + 1} style={styles.hour}>
+            <Text style={styles.hourText}>
+              {format(time, 'HH:mm', {
+                locale: ptBR,
+              })}                        
+            </Text>
+
+            <View style={styles.hourIcons}>
+              <Ionicons name={'pencil-outline'} size={40} color="black" style={styles.icon} onPress={() => handleEditHour(time, index)} />
+              <Ionicons name={'trash-outline'} size={40} color="black" style={styles.icon} onPress={() => handleDeleteHour(index)} />
+            </View>
+          </View>
+        ))}
+      </View>
+      
+      <DateTimePickerModal
+        isVisible={isTimePickerVisible}
+        mode="time"
+        date={currentDate}
+        onConfirm={handleTimePickerConfirm}
+        onCancel={hideTimePicker}
+      />
     </View>
   )   
 }
--- mobile/src/pages/Settings/styles.js
+++ mobile/src/pages/Settings/styles.js
@@ -2,8 +2,80 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center', 
-    alignItems: 'center'
+    marginTop: 100,
+    paddingHorizontal: 20
   },
+
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  headerTitle: {
+    fontWeight: 'bold',
+    fontSize: 22
+  },
+
+  addNewHour: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+    height: 50,
+
+    padding: 30,
+
+    backgroundColor: '#00b803',
+    borderRadius: 50,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+
+  addNewHourText: {
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+
+  containerHours: {    
+    paddingHorizontal: 10
+  },
+
+  noDataFoundText: {
+    fontWeight: 'bold',
+    fontSize: 25,
+
+    marginTop: 200
+  },
+
+  hour: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+
+    marginTop: 50
+  },
+
+  hourText: {
+    fontWeight: 'bold',
+    fontSize: 80,
+  },
+
+  hourIcons: {
+    flexDirection: 'row',
+  },
+
+  icon: {
+    marginHorizontal: 5
+  }
 });
